35ad43d7bab1018041dc37b3299a323f4311407b
Martin Nagy <martin.laszlo.nagy@gmail.com>
2022-03-21 19:35:42 +0100

    Remove controller boilerplate.

---
+++ DictionaryApp.WebApi/Controllers/DictionaryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DictionaryApp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DictionaryController : ControllerBase
+    {
+        private readonly ILogger<DictionaryController> _logger;
+
+        public DictionaryController(ILogger<DictionaryController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
