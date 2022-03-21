b4978f9556f30686d90d052d80bd440137482649
Martin Nagy <martin.laszlo.nagy@gmail.com>
2022-03-21 19:03:27 +0100

    Add database project, model.

---
+++ DictionaryApp.Database/Models/DictionaryEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DictionaryApp.Database.Models
+{
+    public class DictionaryEntry
+    {
+        [Key]
+        public Guid Guid { get; set; }
+        public string English { get; set; }
+        public string Hungarian { get; set; }
+        public string Spanish { get; set; }
+        public string Chinese { get; set; }
+        public string Portugese { get; set; }
+    }
+}
