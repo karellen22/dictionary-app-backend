567eb6218cd7831f7b65d92c5c6061951ecb7bf6
Martin Nagy <martin.laszlo.nagy@gmail.com>
2022-03-21 19:09:50 +0100

    Add application db context.

---
+++ DictionaryApp.Database/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DictionaryApp.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DictionaryApp.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<DictionaryEntry> DictionaryEntries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=DictionaryDB;Trusted_Connection=True");
+        }
+    }
+}
