919c4716eed64f223a5448c783c3f95183b494b9
Martin Nagy <martin.laszlo.nagy@gmail.com>
2022-03-21 23:11:48 +0100

    Add tests for DictionaryController's GetDictionaryEntries method.

---
+++ DictionaryApp.Tests/DictionaryControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using DictionaryApp.Database.Models;
+using DictionaryApp.DataLayer;
+using DictionaryApp.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictionaryApp.Tests
+{
+    public class Tests
+    {
+        private readonly DictionaryController _controller;
+        private readonly IDictionaryServices _service;
+
+        public Tests()
+        {
+            _service = new DictionaryServicesFake();
+            var mock = new Mock<ILogger<DictionaryController>>();
+            ILogger<DictionaryController> logger = mock.Object;
+            _controller = new DictionaryController(logger, _service);
+        }
+
+        [Test]
+        public void Get_WhenMethodCalled_ReturnsOk()
+        {
+            var dictionary = _controller.GetDictionaryEntries();
+            var okObjectResult = dictionary as OkObjectResult;
+            Assert.IsNotNull(okObjectResult);
+        }
+
+        [Test]
+        public void Get_WhenMethodCalled_ReturnsDictionaryEntries()
+        {
+            var dictionary = _controller.GetDictionaryEntries();
+            var okObjectResult = dictionary as OkObjectResult;
+            var dictionaryEntries = okObjectResult.Value as IEnumerable<DictionaryEntry>;
+            Assert.IsNotNull(dictionaryEntries);
+        }
+
+        [Test]
+        public void Get_WhenMethodCalled_ReturnsEveryEntry()
+        {
+            var dictionary = _controller.GetDictionaryEntries();
+            var okObjectResult = dictionary as OkObjectResult;
+            var dictionaryEntries = okObjectResult.Value as IEnumerable<DictionaryEntry>;
+            Assert.AreEqual(5, dictionaryEntries.ToList().Count);
+        }
+    }
+}
---
+++ DictionaryApp.Tests/DictionaryServicesFake.cs
@@ -0,0 +1,35 @@
+﻿using DictionaryApp.Database.Models;
+using DictionaryApp.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DictionaryApp.Tests
+{
+    internal class DictionaryServicesFake : IDictionaryServices
+    {
+        private readonly List<DictionaryEntry> _dictionary;
+        public DictionaryServicesFake()
+        {
+            _dictionary = new List<DictionaryEntry>()
+            {
+                new DictionaryEntry { Guid = Guid.Parse("fed5e007-b9cb-408e-b3df-00b6b5c1d78f"), English = "English", Hungarian = "Hungarian", Spanish = "Spanish", Chinese = "Chinese", Portugese = "Portugese" },
+                new DictionaryEntry { Guid = Guid.Parse("838f049d-8570-4092-9292-4cbc949be4d2"), English = "Agency", Hungarian = "Ügynökség", Spanish = "Agencia", Chinese = "机构", Portugese = "Agência" },
+                new DictionaryEntry { Guid = Guid.Parse("8c58b65a-154e-4406-9766-5e1e91e62597"), English = "Comment", Hungarian = "Megjegyzés", Spanish = "Comentario", Chinese = "备注", Portugese = "Comentário" },
+                new DictionaryEntry { Guid = Guid.Parse("1fa67e0d-dd48-4797-9b10-6bb474d0c0dc"), English = "Comment:", Hungarian = "Megjegyzés:", Spanish = "Comentario:", Chinese = "备注：", Portugese = "Comentário:" },
+                new DictionaryEntry { Guid = Guid.Parse("cbddc998-abbb-498e-b4c2-8619f5665d5d"), English = "Last Name", Hungarian = "Vezetéknév", Spanish = "Apellido", Chinese = "姓氏", Portugese = "Sobrenome" }
+            };
+        }
+
+        public List<DictionaryEntry> GetDictionaryEntries()
+        {
+            return _dictionary;
+        }
+
+        public DictionaryEntry GetEnglishHungarianTranslation(string wordToLookFor)
+        {
+            return _dictionary.FirstOrDefault(entry => entry.English.ToLower() == wordToLookFor.ToLower());
+        }
+    }
+}
