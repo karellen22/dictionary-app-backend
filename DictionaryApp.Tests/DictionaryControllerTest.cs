caa6e5b97444aeeaf3e6eedfcd8e7fd7dd9eb48e
Martin Nagy <martin.laszlo.nagy@gmail.com>
2022-03-21 23:31:56 +0100

    Add tests for GetEnglishHungarianTranslation method.

--- DictionaryApp.Tests/DictionaryControllerTest.cs
+++ DictionaryApp.Tests/DictionaryControllerTest.cs
@@ -25,7 +25,7 @@ namespace DictionaryApp.Tests
         }
 
         [Test]
-        public void Get_WhenMethodCalled_ReturnsOk()
+        public void GetDictionaryEntries_WhenMethodCalled_ReturnsOk()
         {
             var dictionary = _controller.GetDictionaryEntries();
             var okObjectResult = dictionary as OkObjectResult;
@@ -33,7 +33,7 @@ namespace DictionaryApp.Tests
         }
 
         [Test]
-        public void Get_WhenMethodCalled_ReturnsDictionaryEntries()
+        public void GetDictionaryEntries_WhenMethodCalled_ReturnsDictionaryEntries()
         {
             var dictionary = _controller.GetDictionaryEntries();
             var okObjectResult = dictionary as OkObjectResult;
@@ -42,12 +42,44 @@ namespace DictionaryApp.Tests
         }
 
         [Test]
-        public void Get_WhenMethodCalled_ReturnsEveryEntry()
+        public void GetDictionaryEntries_WhenMethodCalled_ReturnsEveryEntry()
         {
             var dictionary = _controller.GetDictionaryEntries();
             var okObjectResult = dictionary as OkObjectResult;
             var dictionaryEntries = okObjectResult.Value as IEnumerable<DictionaryEntry>;
             Assert.AreEqual(5, dictionaryEntries.ToList().Count);
+        }
+
+        [Test]
+        [TestCase("English", "Hungarian")]
+        [TestCase("enGliSh", "Hungarian")]
+        [TestCase("Last Name", "Vezetéknév")]
+        public void GetEnglishHungarianTranslation_WhenMethodCalledWithExistingWord_ReturnsCorrectEntry(string englishWord, string hungarianWord)
+        {
+            var okObjectResult = _controller.GetEnglishHungarianTranslation(englishWord) as OkObjectResult;
+            var dictionaryEntry = okObjectResult.Value as string;
+            Assert.AreEqual(hungarianWord, dictionaryEntry);
+
+        }
+
+        [Test]
+        [TestCase("this one doesn't exist")]
+        [TestCase("this one neither")]
+        public void GetEnglishHungarianTranslation_WhenMethodCalledWithNonExistingWord_ReturnsNonOk(string englishWord)
+        {
+            var dictionaryEntry = _controller.GetEnglishHungarianTranslation(englishWord);
+            Assert.IsInstanceOf<BadRequestObjectResult>(dictionaryEntry);
+        }
+
+        [Test]
+        [TestCase("this one doesn't exist")]
+        [TestCase("this one neither")]
+        public void GetEnglishHungarianTranslation_WhenMethodCalledWithNonExistingWord_ReturnsCorrectErrorMessage(string englishWord)
+        {
+            var dictionaryEntry = _controller.GetEnglishHungarianTranslation(englishWord);
+            var okResult = dictionaryEntry as BadRequestObjectResult;
+
+            Assert.AreEqual($"Word not found in dictionary: {englishWord}", okResult.Value);
         }
     }
 }
